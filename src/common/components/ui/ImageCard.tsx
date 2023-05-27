1e2b5640d9fb3d98d663e080af771c3558fce53e
imdeepss <deepakk@geeconglobal.com>
2023-05-27 19:57:29 +0530

    Fix: add Download button

--- src/common/components/ui/ImageCard.tsx
+++ src/common/components/ui/ImageCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React from 'react'
+import { DownloadButton } from '../form';
 
 type ImageType = {
     src: string;
@@ -10,6 +11,7 @@ const ImageCard = ({ src, alt }: ImageType) => {
         <div className=''>
             <div className='relative'>
                 <Image width={500} height={500} className="h-auto max-w-full rounded-lg" src={src} alt={alt} />
+                <DownloadButton />
             </div>
         </div>
     )
